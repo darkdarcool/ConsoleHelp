6bad2efdbd211522a99fc1421db8dc7721addd86
DELETED ACCOUNT 100% <darrendreddy@gmail.com>
2021-03-04 16:52:39 +0000

    Added extra funds

--- MyApp/setup.py
+++ MyApp/setup.py
@@ -5,13 +5,12 @@ with open("README.md", "r") as fh:
 # delete the url info if error, also change version in an update
 setup(
   name = "ConsoleHelp",
-  version = "0.2.1",
+  version = "0.2.6",
   description = "For all of your console needs!",
   long_description = long_description,
   long_description_content_type = "text/markdown",
   author = "darkdarcool30",
   author_email = "darkdarcool@gmail.com",
-  url = "https://github.com/darkdarcool/ConsoleHelp",
 #To find more licenses or classifiers go to: https://pypi.org/classifiers/
   license = "GNU General Public License v3 (GPLv3)",
   packages=['ConsoleHelp'],
---
+++ test.py
@@ -0,0 +1,5 @@
+import os
+os.system("pip install ConsoleHelp==0.2.6")
+import ConsoleHelp as window
+window.clear()
+window.color.spc("Hello World", 'orange')
